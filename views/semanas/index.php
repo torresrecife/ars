ac539a24fce8bfa61b9cbbd02f2144f12ca6b80b
fabio <fabiotorres.adv@gmail.com>
2026-07-02 18:51:06 -0400

    ARS - Corrigindo a exibiçao das semanas

--- views/semanas/index.php
+++ views/semanas/index.php
@@ -1,30 +1,30 @@
-<div style="margin-top:80px" >
+<div style="margin-top:80px">
 <label><h2><u>Semanas</u></h2></label>
 <div>
 <table class="***REMOVED***list">
 	<tr height="30">
-		<td class="order"><b>CÃƒÂ³digo</b></td>
-		<td class="order"><b>MÃƒÂªs</b></td>
+		<td class="order"><b>Código</b></td>
+		<td class="order"><b>Mês</b></td>
 		<td class="order"><b>Ano</b></td>
-		<td class="order" style="background:#436EEE;color:#ffffff"><b>1Ã‚Âª Semana</b></td>
-		<td class="order" style="background:#436EEE;color:#ffffff"><b>2Ã‚Âª Semana</b></td>
-		<td class="order" style="background:#436EEE;color:#ffffff"><b>3Ã‚Âª Semana</b></td>
-		<td class="order" style="background:#436EEE;color:#ffffff"><b>4Ã‚Âª Semana</b></td>
-		<td class="order" style="background:#1E90FF;color:#ffffff"><b>5Ã‚Âª Semana</b></td>
+		<td class="order" style="background:#436EEE;color:#ffffff"><b>1ª Semana</b></td>
+		<td class="order" style="background:#436EEE;color:#ffffff"><b>2ª Semana</b></td>
+		<td class="order" style="background:#436EEE;color:#ffffff"><b>3ª Semana</b></td>
+		<td class="order" style="background:#436EEE;color:#ffffff"><b>4ª Semana</b></td>
+		<td class="order" style="background:#1E90FF;color:#ffffff"><b>5ª Semana</b></td>
 		<td class="order"><b>Alterado em</b></td>
 		<td class="order"><b>Cadastrado em</b></td>
-		<td class="order">ConfiguraÃƒÂ§ÃƒÂµes</td>
+		<td class="order"><b>Configurações</b></td>
 	</tr>
 <?php foreach ($weeks as $arr): ?>
 	<tr>
 		<td class="order"><?php echo $arr['semanas_id']; ?></td>
 		<td class="order"><?php echo isset($months[$arr['mes']]) ? $months[$arr['mes']] : $arr['mes']; ?></td>
 		<td class="order"><?php echo $arr['ano']; ?></td>
-		<td class="order" style="background:#436EEE;color:#ffffff"><?php echo $arr['ini_1'] . " ÃƒÂ  " . $arr['fim_1']; ?></td>
-		<td class="order" style="background:#436EEE;color:#ffffff"><?php echo $arr['ini_2'] . " ÃƒÂ  " . $arr['fim_2']; ?></td>
-		<td class="order" style="background:#436EEE;color:#ffffff"><?php echo $arr['ini_3'] . " ÃƒÂ  " . $arr['fim_3']; ?></td>
-		<td class="order" style="background:#436EEE;color:#ffffff"><?php echo $arr['ini_4'] . " ÃƒÂ  " . $arr['fim_4']; ?></td>
-		<td class="order" style="background:#1E90FF;color:#ffffff"><?php echo ($arr['ini_5'] ? $arr['ini_5'] . " ÃƒÂ  " . $arr['fim_5'] : '-'); ?></td>
+		<td class="order" style="background:#436EEE;color:#ffffff"><?php echo $arr['ini_1'] . "&nbsp;à&nbsp;" . $arr['fim_1']; ?></td>
+		<td class="order" style="background:#436EEE;color:#ffffff"><?php echo $arr['ini_2'] . "&nbsp;à&nbsp;" . $arr['fim_2']; ?></td>
+		<td class="order" style="background:#436EEE;color:#ffffff"><?php echo $arr['ini_3'] . "&nbsp;à&nbsp;" . $arr['fim_3']; ?></td>
+		<td class="order" style="background:#436EEE;color:#ffffff"><?php echo $arr['ini_4'] . "&nbsp;à&nbsp;" . $arr['fim_4']; ?></td>
+		<td class="order" style="background:#1E90FF;color:#ffffff"><?php echo ($arr['ini_5'] ? $arr['ini_5'] . "&nbsp;à&nbsp;" . $arr['fim_5'] : '-'); ?></td>
 		<td class="order"><?php echo $arr['dataalt']; ?></td>
 		<td class="order"><?php echo $arr['datacad']; ?></td>
 		<td class="order" style="width:130px"><?php echo fc_botoes_sem($arr['semanas_id'], 'block', $arr['mes']); ?></td>
@@ -37,43 +37,43 @@
 		<div>
 			<table style="width:400px">
 				<tr>
-					<td><label>CÃƒÂ³digo:</label></td>
+					<td><label>Código:</label></td>
 					<td colspan="3"><input type="text" class="cls_sem" name="id_sem" id="id_sem" style="border:0;background:#fff;width:50px" title="Id da semana" readonly="readonly" /></td>
 				</tr>
 				<tr>
-					<td style="width:50px"><label>MÃƒÂªs/Ano:</label></td>
-					<td style="width:50px"><input type="text" class="cls_sem" name="mes_sem" id="mes_sem" style="width:90%" title="MÃƒÂªs" alt="integer"/></td>
+					<td style="width:50px"><label>Mês/Ano:</label></td>
+					<td style="width:50px"><input type="text" class="cls_sem" name="mes_sem" id="mes_sem" style="width:90%" title="Mês" alt="integer"/></td>
 					<td align="center" style="width:50px">/</td>
 					<td style="width:50px"><input type="text" class="cls_sem" name="ano_sem" id="ano_sem" style="width:90%" title="Ano" alt="integer"/></td>
 				<tr>
-					<td><label>1Ã‚Âª Semana:</label></td>
-					<td><input type="text" class="cls_sem" name="ini1_sem" id="ini1_sem" style="width:90%" title="1Ã‚Âª Semana inÃƒÂ­cio" alt="integer"/></td>
-					<td align="center" style="width:50px">ÃƒÂ </td>
-					<td><input type="text" class="cls_sem" name="fim1_sem" id="fim1_sem" style="width:90%" title="1Ã‚Âª Semana fim" alt="integer"/></td>
+					<td><label>1ª Semana:</label></td>
+					<td><input type="text" class="cls_sem" name="ini1_sem" id="ini1_sem" style="width:90%" title="1ª Semana início" alt="integer"/></td>
+					<td align="center" style="width:50px">à</td>
+					<td><input type="text" class="cls_sem" name="fim1_sem" id="fim1_sem" style="width:90%" title="1ª Semana fim" alt="integer"/></td>
 				</tr>
 				<tr>
-					<td><label>2Ã‚Âª Semana:</label></td>
-					<td><input type="text" class="cls_sem" name="ini2_sem" id="ini2_sem" style="width:90%" title="2Ã‚Âª Semana inÃƒÂ­cio" alt="integer"/></td>
-					<td align="center" style="width:50px">ÃƒÂ </td>
-					<td><input type="text" class="cls_sem" name="fim2_sem" id="fim2_sem" style="width:90%" title="3Ã‚Âª Semana fim" alt="integer"/></td>
+					<td><label>2ª Semana:</label></td>
+					<td><input type="text" class="cls_sem" name="ini2_sem" id="ini2_sem" style="width:90%" title="2ª Semana início" alt="integer"/></td>
+					<td align="center" style="width:50px">à</td>
+					<td><input type="text" class="cls_sem" name="fim2_sem" id="fim2_sem" style="width:90%" title="2ª Semana fim" alt="integer"/></td>
 				</tr>
 				<tr>
-					<td><label>3Ã‚Âª Semana:</label></td>
-					<td><input type="text" class="cls_sem" name="ini3_sem" id="ini3_sem" style="width:90%" title="3Ã‚Âª Semana inÃƒÂ­cio" alt="integer"/></td>
-					<td align="center" style="width:50px">ÃƒÂ </td>
-					<td><input type="text" class="cls_sem" name="fim3_sem" id="fim3_sem" style="width:90%" title="3Ã‚Âª Semana fim" alt="integer"/></td>
+					<td><label>3ª Semana:</label></td>
+					<td><input type="text" class="cls_sem" name="ini3_sem" id="ini3_sem" style="width:90%" title="3ª Semana início" alt="integer"/></td>
+					<td align="center" style="width:50px">à</td>
+					<td><input type="text" class="cls_sem" name="fim3_sem" id="fim3_sem" style="width:90%" title="3ª Semana fim" alt="integer"/></td>
 				</tr>
 				<tr>
-					<td><label>4Ã‚Âª Semana:</label></td>
-					<td><input type="text" class="cls_sem" name="ini4_sem" id="ini4_sem" style="width:90%" title="4Ã‚Âª Semana inÃƒÂ­cio" alt="integer"/></td>
-					<td align="center" style="width:50px">ÃƒÂ </td>
-					<td><input type="text" class="cls_sem" name="fim4_sem" id="fim4_sem" style="width:90%" title="4Ã‚Âª Semana fim" alt="integer"/></td>
+					<td><label>4ª Semana:</label></td>
+					<td><input type="text" class="cls_sem" name="ini4_sem" id="ini4_sem" style="width:90%" title="4ª Semana início" alt="integer"/></td>
+					<td align="center" style="width:50px">à</td>
+					<td><input type="text" class="cls_sem" name="fim4_sem" id="fim4_sem" style="width:90%" title="4ª Semana fim" alt="integer"/></td>
 				</tr>
 				<tr>
-					<td><label>5Ã‚Âª Semana:</label></td>
-					<td><input type="text" class="cls_sem" name="ini5_sem" id="ini5_sem" style="width:90%" title="5Ã‚Âª Semana inÃƒÂ­cio" alt="integer"/></td>
-					<td align="center" style="width:50px">ÃƒÂ </td>
-					<td><input type="text" class="cls_sem" name="fim5_sem" id="fim5_sem" style="width:90%" title="6Ã‚Âª Semana fim" alt="integer"/></td>
+					<td><label>5ª Semana:</label></td>
+					<td><input type="text" class="cls_sem" name="ini5_sem" id="ini5_sem" style="width:90%" title="5ª Semana início" alt="integer"/></td>
+					<td align="center" style="width:50px">à</td>
+					<td><input type="text" class="cls_sem" name="fim5_sem" id="fim5_sem" style="width:90%" title="5ª Semana fim" alt="integer"/></td>
 				</tr>
 			</table>
 		</div>
