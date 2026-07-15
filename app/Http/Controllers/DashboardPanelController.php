c918230b28395cd2e1d6340c9814c941f04221e2
fabio <fabiotorres.adv@gmail.com>
2026-07-15 14:40:12 -0400

    ARS - foi removido o inc/seguranca.php e migrado para Blade: resources/views/dashboard/panel.blade.php,resources/views/geral/monthly.blade.php,    resources/views/geral/weekly.blade.php

--- app/Http/Controllers/DashboardPanelController.php
+++ app/Http/Controllers/DashboardPanelController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Services\DashboardPanelService;
+use App\Support\View;
 use Illuminate\Http\Request;
 
 class DashboardPanelController extends Controller
@@ -12,18 +13,19 @@ class DashboardPanelController extends Controller
 	/** @var DashboardPanelService */
 	private $service;
 
-	public function __construct(DashboardPanelService $service)
+	/** @var View */
+	private $view;
+
+	public function __construct(DashboardPanelService $service, View $view)
 	{
 		$this->service = $service;
+		$this->view = $view;
 	}
 
 	public function index(array $input = array(), array $session = array())
 	{
 		$viewData = $this->service->build($input, $session);
-
-		ob_start();
-		include base_path('views/dashboard/panel.php');
-		return (string) ob_get_clean();
+		return $this->view->render('dashboard/panel', array('viewData' => $viewData));
 	}
 
 	public function webIndex(Request $request)
