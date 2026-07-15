29917477ae18f5941679ae6b251cb85aa6c5c1b3
fabio <fabiotorres.adv@gmail.com>
2026-07-15 17:27:26 -0400

    ARS - limpeza estrutural: • padronizar rotas GET onde ainda aceitam POST sem necessidade • depois padronizar controllers e JSON no estilo Laravel.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -18,20 +18,5 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    protected $except = [
-        'index',
-        'index.php',
-        'carteiras',
-        'painel',
-        'producao',
-        'relatorio',
-        'admin',
-        'usuarios',
-        'setores',
-        'clientes',
-        'andamentos',
-        'metas',
-        'semanas',
-        'regioes',
-    ];
+    protected $except = [];
 }
