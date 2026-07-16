473752a34f524519a33b98e189a98fba8db77368
fabio <fabiotorres.adv@gmail.com>
2026-07-16 19:59:34 -0400

    ARS - correçao para a inserçao de uma nova meta e a exclusão

--- resources/views/index/metas-select.blade.php
+++ resources/views/index/metas-select.blade.php
@@ -12,12 +12,12 @@
 			<label for="startDate">M&ecirc;s / Ano:</label>
 			<input type="text" name="startDate" id="startDate" class="date-picker" readonly="readonly" value="{{ e($monthYearLabel) }}" style="font-family:Tahoma"/>
 			<span id="obg_date"></span>
-			<input type="hidden" name="mes" id="mes" value="{{ date('m') }}"/>
-			<input type="hidden" name="ano" id="ano" value="{{ date('Y') }}"/>
+			<input type="hidden" name="mes" id="mes" value="{{ e((string) $month) }}"/>
+			<input type="hidden" name="ano" id="ano" value="{{ e((string) $year) }}"/>
 			<br><br><br><br><br>
 			<div class="icon-wrapper">
 				<div class="icon">
-					<a href="#" id="frm" onclick="EnviarPagina('metas',true,'',''); return false;">
+					<a href="#" id="frm" onclick="AbrirMetasSelecao(); return false;">
 						<img src="css/images/header/icon-48-themes.png" alt="" /><span>Metas</span>
 					</a>
 				</div>
