ee269bd067039b1126cb2ab2e6c591da11450469
fabio <fabiotorres.adv@gmail.com>
2026-07-24 10:04:03 -0400

    ARS - Correçao para o botão exportar para excel

--- js/modules/details.js
+++ js/modules/details.js
@@ -15,3 +15,68 @@ $(document).ready(function () {
 function enviar_neo(valor) {
 	window.open('http://192.168.81.200/Modulos/ElementosProcessuais/ProcessoFichaGeral.aspx?idProcesso=' + valor);
 }
+
+function exportDetailTable(tableId, fileName) {
+	var sourceTable = document.getElementById(tableId);
+	if (!sourceTable) {
+		return;
+	}
+
+	var exportTable = sourceTable.cloneNode(true);
+	var sourceRows = sourceTable.getElementsByTagName('tr');
+	var exportRows = exportTable.getElementsByTagName('tr');
+	var visibleRows = [];
+	var i;
+
+	for (i = 0; i < sourceRows.length; i++) {
+		if ($(sourceRows[i]).is(':visible')) {
+			visibleRows.push(exportRows[i]);
+		}
+	}
+
+	while (exportTable.firstChild) {
+		exportTable.removeChild(exportTable.firstChild);
+	}
+
+	for (i = 0; i < visibleRows.length; i++) {
+		exportTable.appendChild(visibleRows[i]);
+	}
+
+	$(exportTable).find('select').each(function () {
+		var selected = $(this).find('option:selected').text() || $(this).find('option').first().text() || '';
+		$(this).replaceWith(document.createTextNode(selected));
+	});
+
+	$(exportTable).find('[onclick]').removeAttr('onclick');
+
+	var html = '<html xmlns:o="urn:schemas-microsoft-com:office:office" ' +
+		'xmlns:x="urn:schemas-microsoft-com:office:excel" ' +
+		'xmlns="http://www.w3.org/TR/REC-html40"><head>' +
+		'<meta charset="utf-8"></head><body>' +
+		exportTable.outerHTML +
+		'</body></html>';
+
+	var blob = new Blob(['\ufeff', html], {
+		type: 'application/vnd.ms-excel;charset=utf-8;'
+	});
+
+	var safeFileName = (fileName || 'detail-report')
+		.replace(/[\\/:*?"<>|]+/g, '-')
+		.replace(/\s+/g, '-');
+
+	if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+		window.navigator.msSaveOrOpenBlob(blob, safeFileName + '.xls');
+		return;
+	}
+
+	var url = window.URL.createObjectURL(blob);
+	var link = document.createElement('a');
+	link.href = url;
+	link.download = safeFileName + '.xls';
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+	window.setTimeout(function () {
+		window.URL.revokeObjectURL(url);
+	}, 1000);
+}
